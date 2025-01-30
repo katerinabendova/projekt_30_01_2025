d5e0a3429322c1b287d1cec73d05b8dd10ba8789
bendova <xkaterinabendova@seznam.cz>
2025-01-30 10:11:04 +0100

    pridane settry a metoda addPlant

--- src/Flower.java
+++ src/Flower.java
@@ -4,6 +4,34 @@ public abstract class Flower {
     protected double price;
     protected double neededArea;
     protected double chanceOfGrowth;
+    protected int plantNumber;
+    protected double plotSize;
+    protected double freePlace;
+
+    public void setPlotSize(double plotSize) {
+        this.plotSize = 10000; //m^2
+    }
+
+    public void setPlantNumber(int plantNumber) {
+        this.plantNumber = 5;
+    }
+
+    public void setFreePlace(double freePlace) {
+        this.freePlace = plotSize -= neededArea;
+    }
+
+    public boolean addPlant( int plantNumber, double freePlace){
+
+        if (freePlace == 0) {
+            return false;
+        }
+        if (plantNumber >= 5){
+            return false;
+        }
+        return true;
+    }
+
+
 
     @Override
     public String toString() {
